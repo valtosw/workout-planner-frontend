898651b78fe665dcf1f23616d5c29b575d32b002
valtosw <volodijbessarabov@knu.ua>
2025-02-21 13:14:21 +0200

    replace the default layout with landing default layout

---
+++ src/layouts/landing-default.tsx
@@ -0,0 +1,26 @@
+import { Link } from "@heroui/link";
+
+import { LandingNavbar } from "@/components/landing-navbar";
+
+export default function LandingDefaultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative flex flex-col h-screen">
+      <LandingNavbar />
+      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
+        {children}
+      </main>
+      <footer className="w-full flex items-center justify-center py-3">
+        <Link
+          isExternal
+          className="flex items-center gap-1 text-current"
+          href="https://heroui.com"
+          title="heroui.com homepage"
+        />
+      </footer>
+    </div>
+  );
+}
