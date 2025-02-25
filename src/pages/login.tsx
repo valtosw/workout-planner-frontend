bdb765d9ef696f62c9f18b652c3792a4e9d8a957
valtosw <volodijbessarabov@knu.ua>
2025-02-25 23:13:24 +0200

    feat: add LoginPage component to render LoginForm within LandingDefaultLayout

---
+++ src/pages/login.tsx
@@ -0,0 +1,10 @@
+import { LoginForm } from "@/components/login-form";
+import LandingDefaultLayout from "@/layouts/landing-default";
+
+export default function LoginPage() {
+  return (
+    <LandingDefaultLayout>
+      <LoginForm />
+    </LandingDefaultLayout>
+  );
+}
