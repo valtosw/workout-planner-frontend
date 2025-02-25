2a210ec144f4dc8b86dcede793ac7dba0fda0489
valtosw <volodijbessarabov@knu.ua>
2025-02-25 23:12:41 +0200

    feat: add Signup and Login pages to routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 
-import TrainersPage from "./pages/trainers";
-
+import TrainersPage from "@/pages/trainers";
+import SignupPage from "@/pages/signup";
 import IndexPage from "@/pages/index";
 import DocsPage from "@/pages/docs";
 import PricingPage from "@/pages/pricing";
 import BlogPage from "@/pages/blog";
 import AboutPage from "@/pages/about";
+import LoginPage from "@/pages/login";
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
       <Route element={<BlogPage />} path="/blog" />
       <Route element={<AboutPage />} path="/about" />
       <Route element={<TrainersPage />} path="/trainers" />
+      <Route element={<SignupPage />} path="/signup" />
+      <Route element={<LoginPage />} path="/login" />
     </Routes>
   );
 }
