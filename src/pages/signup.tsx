e67e7d8201a9ed9019daa40241633a7775c96c9c
valtosw <volodijbessarabov@knu.ua>
2025-02-25 23:13:29 +0200

    feat: add SignupPage component to render SignUpForm within LandingDefaultLayout

---
+++ src/pages/signup.tsx
@@ -0,0 +1,10 @@
+import LandingDefaultLayout from "@/layouts/landing-default";
+import { SignUpForm } from "@/components/signup-form";
+
+export default function SignupPage() {
+  return (
+    <LandingDefaultLayout>
+      <SignUpForm />
+    </LandingDefaultLayout>
+  );
+}
