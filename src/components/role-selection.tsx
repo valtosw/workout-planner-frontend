0e90318190784f24171e404061bab06f84194ebe
valtosw <volodijbessarabov@knu.ua>
2025-02-26 13:37:50 +0200

    refactor: remove unused useEffect import in role-selection component

--- src/components/role-selection.tsx
+++ src/components/role-selection.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Icon } from "@iconify/react";
 import { useTheme } from "@heroui/use-theme";
 import { Card } from "@heroui/react";
