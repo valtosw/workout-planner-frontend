9c26dd479a0d64c41f373347f5921c05fe5dc27e
valtosw <volodijbessarabov@knu.ua>
2025-02-27 12:23:33 +0200

    feat: add axiosPrivate instance for private API requests

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,3 +5,9 @@ export const BASE_URL = "http://localhost:5269/api";
 export default axios.create({
   baseURL: BASE_URL,
 });
+
+export const axiosPrivate = axios.create({
+  baseURL: BASE_URL,
+  headers: { "Content-Type": "application/json" },
+  withCredentials: true,
+});
