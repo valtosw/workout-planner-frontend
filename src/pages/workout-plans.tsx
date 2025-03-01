0c8ddf06dc2086d0e8792dac30ced173357f7dcb
valtosw <volodijbessarabov@knu.ua>
2025-03-01 22:34:46 +0200

    feat: add WorkoutPlansPage component with layout and CreateWorkoutPlanCard

---
+++ src/pages/workout-plans.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+
+import DefaultLayout from "@/layouts/default";
+import { CreateWorkoutPlanCard } from "@/components/create-workout-plan";
+
+const WorkoutPlansPage: React.FC = () => {
+  return (
+    <DefaultLayout>
+      <h1 className="text-3xl font-extrabold tracking-wide text-gray-900 dark:text-gray-100 mb-4">
+        Your Workout Plans
+      </h1>
+      <div className="h-full flex items-start min-h-screen ">
+        <CreateWorkoutPlanCard />
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default WorkoutPlansPage;
