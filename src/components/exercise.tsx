bd1187bc992590534615aa63448decc48810afee
valtosw <volodijbessarabov@knu.ua>
2025-03-01 22:34:19 +0200

    feat: add ExerciseBlock component skeleton with Autocomplete import

---
+++ src/components/exercise.tsx
@@ -0,0 +1,9 @@
+// import { Autocomplete } from "@heroui/react";
+
+// export const ExerciseBlock = () => {
+//     return (
+//         <div className="">
+//             <Autocomplete 
+//         </div>
+//     );
+// }
