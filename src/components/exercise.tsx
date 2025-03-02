2192738f5a43050a589bd3db28f2173b3eef8f52
valtosw <volodijbessarabov@knu.ua>
2025-03-02 12:41:01 +0200

    refactor: remove unused components for exercise, role selection, create workout plan, and login form

--- src/components/exercise.tsx
+++
@@ -1,9 +0,0 @@
-// import { Autocomplete } from "@heroui/react";
-
-// export const ExerciseBlock = () => {
-//     return (
-//         <div className="">
-//             <Autocomplete 
-//         </div>
-//     );
-// }
