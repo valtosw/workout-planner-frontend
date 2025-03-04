58ce5177d7db93789bbd091f6b573330762839ea
valtosw <volodijbessarabov@knu.ua>
2025-03-04 20:12:36 +0200

    feat: implement lazy loading for Signup and Login pages with Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
 import { Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
+import React from "react";
 
+import { LoadingPage } from "@/pages/status-pages/loading-page";
+import { NotFoundPage } from "@/pages/status-pages/not-found-page";
 import TrainersPage from "@/pages/trainers";
-import SignupPage from "@/pages/signup";
 import IndexPage from "@/pages/index";
-import DocsPage from "@/pages/docs";
-import PricingPage from "@/pages/pricing";
-import BlogPage from "@/pages/blog";
-import AboutPage from "@/pages/about";
-import LoginPage from "@/pages/login";
 import WorkoutPlansPage from "@/pages/workout-plans";
 import { ROUTES } from "@/constants/routes";
 
+const SignupPage = React.lazy(() => import("@/pages/signup"));
+const LoginPage = React.lazy(() => import("@/pages/login"));
+
 function App() {
   return (
-    <Routes>
-      <Route element={<IndexPage />} path="/" />
-      <Route element={<DocsPage />} path="/docs" />
-      <Route element={<PricingPage />} path="/pricing" />
-      <Route element={<BlogPage />} path="/blog" />
-      <Route element={<AboutPage />} path="/about" />
-      <Route element={<TrainersPage />} path={ROUTES.TRAINERS} />
-      <Route element={<WorkoutPlansPage />} path={ROUTES.WORKOUT_PLANS} />
-      <Route element={<SignupPage />} path={ROUTES.SIGNUP} />
-      <Route element={<LoginPage />} path={ROUTES.LOGIN} />
-    </Routes>
+    <Suspense fallback={<LoadingPage />}>
+      <Routes>
+        <Route element={<IndexPage />} path="/" />
+        <Route element={<TrainersPage />} path={ROUTES.TRAINERS} />
+        <Route element={<WorkoutPlansPage />} path={ROUTES.WORKOUT_PLANS} />
+        <Route element={<SignupPage />} path={ROUTES.SIGNUP} />
+        <Route element={<LoginPage />} path={ROUTES.LOGIN} />
+        <Route element={<NotFoundPage />} path="*" />
+      </Routes>
+    </Suspense>
   );
 }
 
