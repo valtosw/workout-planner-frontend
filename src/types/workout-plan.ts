f4776928fc2eba3f73ec2687a9ae913ffe77575e
valtosw <volodijbessarabov@knu.ua>
2025-03-04 20:11:53 +0200

    feat: add TypeScript interfaces for WorkoutPlan and related entities

---
+++ src/types/workout-plan.ts
@@ -0,0 +1,32 @@
+export interface WorkoutPlan {
+  name: string;
+  createdBy: CreatedBy;
+  assignedTo: AssignedTo | null;
+  workoutPlanEntries: WorkoutPlanEntry[];
+}
+
+export interface WorkoutPlanEntry {
+  reps: number;
+  sets: number;
+  weight: number;
+  exercise: string;
+  muscleGroup: string;
+}
+
+export interface CreatedBy {
+  firstName: string;
+  lastName: string;
+  email: string;
+  profilePicture: string | null;
+}
+
+export interface AssignedTo {
+  firstName: string;
+  lastName: string;
+  email: string;
+  profilePicture: string | null;
+}
+
+export interface WorkoutPlanProps {
+  workoutPlan: WorkoutPlan;
+}
