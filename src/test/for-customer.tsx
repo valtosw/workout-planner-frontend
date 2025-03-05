3d259d19972bc900a4f415116172a82383c783f9
valtosw <volodijbessarabov@knu.ua>
2025-03-05 23:06:52 +0200

    feat: add For Customer page with theme switch component

---
+++ src/test/for-customer.tsx
@@ -0,0 +1,10 @@
+import { ThemeSwitch } from "@/components/theme-switch";
+
+export default function ForCustomerPage() {
+  return (
+    <div>
+      <h1>For Customer Page</h1>
+      <ThemeSwitch />
+    </div>
+  );
+}
