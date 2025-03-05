fd83add55d73380b325ba21660aa3087eba5d662
valtosw <volodijbessarabov@knu.ua>
2025-03-05 23:07:27 +0200

    feat: update WorkoutPlansPage to use authenticated user ID for WorkoutPlanList

--- src/pages/workout-plans.tsx
+++ src/pages/workout-plans.tsx
@@ -3,8 +3,11 @@ import { Spacer } from "@heroui/react";
 import DefaultLayout from "@/layouts/default";
 import { CreateWorkoutPlanCard } from "@/components/workout-plan-components/create-workout-plan";
 import { WorkoutPlanList } from "@/components/workout-plan-components/workout-plan-list";
+import useAuth from "@/hooks/useAuth";
 
 const WorkoutPlansPage: React.FC = () => {
+  const { auth } = useAuth();
+
   return (
     <DefaultLayout>
       <h1 className="text-3xl font-extrabold tracking-wide text-gray-900 dark:text-gray-100 mb-4">
@@ -14,7 +17,7 @@ const WorkoutPlansPage: React.FC = () => {
         <CreateWorkoutPlanCard />
         <Spacer x={4} />
 
-        <WorkoutPlanList userId="08d2316e-f7a8-11ef-a1a6-98fa9b267c16" />
+        <WorkoutPlanList userId={auth?.id} />
       </div>
     </DefaultLayout>
   );
