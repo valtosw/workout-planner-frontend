9e04051b4cfe842b5efb2e1e7f865b59499a0550
valtosw <volodijbessarabov@knu.ua>
2025-03-05 23:07:31 +0200

    feat: add AuthProvider context for managing authentication and user data

---
+++ src/context/AuthProvider.tsx
@@ -0,0 +1,85 @@
+import { createContext, useState, ReactNode, FC, useEffect } from "react";
+
+import axiosPrivate from "../api/axios";
+export interface AuthData {
+  accessToken: string;
+  id: string;
+  email: string;
+}
+
+export interface UserData {
+  fullName: string;
+  profilePicture: string;
+}
+
+export interface AuthContextType {
+  auth: AuthData | null;
+  user: UserData | null;
+  setAuth: React.Dispatch<React.SetStateAction<AuthData | null>>;
+  setUser: React.Dispatch<React.SetStateAction<UserData | null>>;
+  persist: boolean;
+  togglePersist: () => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthProvider: FC<AuthProviderProps> = ({ children }) => {
+  const [auth, setAuth] = useState<AuthData | null>(null);
+  const [user, setUser] = useState<UserData | null>(null);
+  const [persist, setPersist] = useState<boolean>(() => {
+    return localStorage.getItem("persist") === "true";
+  });
+
+  const togglePersist = () => {
+    const newPersist = !persist;
+
+    setPersist(newPersist);
+    localStorage.setItem("persist", newPersist ? "true" : "false");
+  };
+
+  const logout = async () => {
+    try {
+      await axiosPrivate.post("/Auth/Logout", {}, { withCredentials: true });
+      setAuth(null);
+      setUser(null);
+      console.log("Logged out");
+    } catch (err) {
+      console.error("Logout Error:", err);
+    }
+  };
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      console.log("Auth:", auth);
+      if (auth?.id) {
+        try {
+          const response = await axiosPrivate.get(
+            `/ApplicationUser/${auth.id}`,
+          );
+
+          console.log("User data:", response.data);
+          setUser(response.data);
+        } catch (err) {
+          console.error("Error fetching user data:", err);
+        }
+      }
+    };
+
+    fetchUserData();
+  }, [auth]);
+
+  return (
+    <AuthContext.Provider
+      value={{ auth, user, setAuth, setUser, persist, togglePersist, logout }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
