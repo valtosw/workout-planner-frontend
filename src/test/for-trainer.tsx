2b785935d850240c57f59bc1147f36d158cc2178
valtosw <volodijbessarabov@knu.ua>
2025-03-05 23:06:48 +0200

    feat: add For Trainer page with theme switch component

---
+++ src/test/for-trainer.tsx
@@ -0,0 +1,10 @@
+import { ThemeSwitch } from "@/components/theme-switch";
+
+export default function ForTrainerPage() {
+  return (
+    <div>
+      <h1>For Trainer Page</h1>
+      <ThemeSwitch />
+    </div>
+  );
+}
