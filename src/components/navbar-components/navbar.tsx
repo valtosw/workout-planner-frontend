de3cc9a2d1934f8ae76f1d8300ab8c5dddb2c648
valtosw <volodijbessarabov@knu.ua>
2025-03-07 12:47:43 +0200

    feat: update Navbar to display user information and implement logout functionality

--- src/components/navbar-components/navbar.tsx
+++ src/components/navbar-components/navbar.tsx
@@ -14,17 +14,20 @@ import {
   Dropdown,
   DropdownMenu,
   Avatar,
+  Button,
 } from "@heroui/react";
 
 import { ThemeSwitch } from "../theme-switch";
 
 import { Logo } from "@/components/icons";
+import useAuth from "@/hooks/useAuth";
 
 const MenuItems = ["Profile", "Workout Plans", "Trainers", "Log Out"];
 
 export const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   const location = useLocation();
+  const { user, auth, logout } = useAuth();
 
   return (
     <HeroUINavbar maxWidth="full" onMenuOpenChange={setIsMenuOpen}>
@@ -72,16 +75,16 @@ export const Navbar = () => {
               isBordered
               as="button"
               className="transition-transform"
-              color="secondary"
-              name="Jason Hughes"
-              size="sm"
-              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+              color="default"
+              name={user?.fullName}
+              size="md"
+              src={user?.profilePicture}
             />
           </DropdownTrigger>
           <DropdownMenu aria-label="Profile Actions" variant="flat">
             <DropdownItem key="profile" className="h-14 gap-2">
               <p className="font-semibold">Signed in as</p>
-              <p className="font-semibold">zoey@example.com</p>
+              <p className="font-semibold">{auth?.email}</p>
             </DropdownItem>
             <DropdownItem key="settings">My Settings</DropdownItem>
             <DropdownItem key="team_settings">Team Settings</DropdownItem>
@@ -89,7 +92,7 @@ export const Navbar = () => {
             <DropdownItem key="system">System</DropdownItem>
             <DropdownItem key="configurations">Configurations</DropdownItem>
             <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-            <DropdownItem key="logout" color="danger">
+            <DropdownItem key="logout" color="danger" onPress={logout}>
               Log Out
             </DropdownItem>
           </DropdownMenu>
