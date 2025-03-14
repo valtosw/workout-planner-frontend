ac9540cb454700805c39a59748ba2b89511087ec
valtosw <volodijbessarabov@knu.ua>
2025-03-14 21:04:02 +0200

    feat: add routes for ForgotPasswordPage and ResetPasswordPage in App component

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import PersistLogin from "./components/auth-components/persist-login";
 import ForCustomerPage from "./test/for-customer";
 import ProtectedRoute from "./components/auth-components/protected-route";
 import ForTrainerPage from "./test/for-trainer";
+import { EmailConfirmationPage } from "./pages/status-pages/email-confirmation";
 
 import { LoadingPage } from "@/pages/status-pages/loading-page";
 import { NotFoundPage } from "@/pages/status-pages/not-found-page";
@@ -14,10 +15,11 @@ import TrainersPage from "@/pages/trainers";
 import IndexPage from "@/pages/index";
 import WorkoutPlansPage from "@/pages/workout-plans";
 import { ROUTES } from "@/constants/routes";
-import { EmailConfirmationPage } from "./pages/status-pages/email-confirmation";
 
 const SignupPage = React.lazy(() => import("@/pages/signup"));
 const LoginPage = React.lazy(() => import("@/pages/login"));
+const ForgotPasswordPage = React.lazy(() => import("@/pages/forgot-password"));
+const ResetPasswordPage = React.lazy(() => import("@/pages/reset-password"));
 
 function App() {
   return (
@@ -28,6 +30,8 @@ function App() {
         <Route element={<NotFoundPage />} path="*" />
         <Route element={<LoginPage />} path={ROUTES.LOGIN} />
         <Route element={<SignupPage />} path={ROUTES.SIGNUP} />
+        <Route element={<ForgotPasswordPage />} path={ROUTES.FORGOT_PASSWORD} />
+        <Route element={<ResetPasswordPage />} path={ROUTES.RESET_PASSWORD} />
         <Route element={<PersistLogin />}>
           <Route element={<ProtectedRoute allowedRoles={["Customer"]} />}>
             <Route element={<ForCustomerPage />} path="/for-customer" />
