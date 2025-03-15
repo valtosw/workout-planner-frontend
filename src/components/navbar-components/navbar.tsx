3ea773536005ab2b1ea094ac84929f63fb3c6063
valtosw <volodijbessarabov@knu.ua>
2025-03-15 11:54:46 +0200

    feat: implement conditional rendering of AuthorizedNavbar and UnauthorizedNavbar based on authentication state

--- src/components/navbar-components/navbar.tsx
+++ src/components/navbar-components/navbar.tsx
@@ -1,119 +1,17 @@
-import React from "react";
 import { useLocation } from "react-router-dom";
-import {
-  Navbar as HeroUINavbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  NavbarMenuToggle,
-  NavbarMenu,
-  NavbarMenuItem,
-  Link,
-  DropdownItem,
-  DropdownTrigger,
-  Dropdown,
-  DropdownMenu,
-  Avatar,
-} from "@heroui/react";
-import { Link as RouterLink } from "react-router-dom";
 
-import { ThemeSwitch } from "../theme-switch";
+import { AuthorizedNavbar } from "./authorized-navbar";
+import { UnauthorizedNavbar } from "./unauthorized-navbar";
 
-import { Logo } from "@/components/icons";
 import useAuth from "@/hooks/useAuth";
 
-const MenuItems = ["Profile", "Workout Plans", "Trainers", "Log Out"];
-
 export const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
   const location = useLocation();
-  const { user, auth, logout } = useAuth();
+  const { auth } = useAuth();
 
-  return (
-    <HeroUINavbar maxWidth="full" onMenuOpenChange={setIsMenuOpen}>
-      <NavbarContent>
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="sm:hidden"
-        />
-        <NavbarBrand>
-          <Logo />
-          <p className="font-bold text-inherit">Workout Planner</p>
-        </NavbarBrand>
-      </NavbarContent>
-
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem isActive={location.pathname === "/workout-plans"}>
-          <Link
-            as={RouterLink}
-            color={
-              location.pathname === "/workout-plans"
-                ? "secondary"
-                : "foreground"
-            }
-            to="/workout-plans"
-          >
-            Workout Plans
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive={location.pathname === "/trainers"}>
-          <Link
-            as={RouterLink}
-            color={
-              location.pathname === "/trainers" ? "secondary" : "foreground"
-            }
-            to="/trainers"
-          >
-            Trainers
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-
-      <NavbarContent as="div" justify="end">
-        <ThemeSwitch />
-        <Dropdown placement="bottom-end">
-          <DropdownTrigger>
-            <Avatar
-              isBordered
-              as="button"
-              className="transition-transform"
-              color="default"
-              name={user?.fullName}
-              size="md"
-              src={user?.profilePicture}
-            />
-          </DropdownTrigger>
-          <DropdownMenu aria-label="Profile Actions" variant="flat">
-            <DropdownItem key="profile" className="h-14 gap-2">
-              <p className="font-semibold">Signed in as</p>
-              <p className="font-semibold">{auth?.email}</p>
-            </DropdownItem>
-            <DropdownItem key="settings">My Settings</DropdownItem>
-            <DropdownItem key="team_settings">Team Settings</DropdownItem>
-            <DropdownItem key="analytics">Analytics</DropdownItem>
-            <DropdownItem key="system">System</DropdownItem>
-            <DropdownItem key="configurations">Configurations</DropdownItem>
-            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
-            <DropdownItem key="logout" color="danger" onPress={logout}>
-              Log Out
-            </DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </NavbarContent>
-      <NavbarMenu>
-        {MenuItems.map((item, index) => (
-          <NavbarMenuItem key={`${item}-${index}`}>
-            <Link
-              className="w-full"
-              color={index === MenuItems.length - 1 ? "danger" : "foreground"}
-              href="#"
-              size="lg"
-            >
-              {item}
-            </Link>
-          </NavbarMenuItem>
-        ))}
-      </NavbarMenu>
-    </HeroUINavbar>
+  return auth ? (
+    <AuthorizedNavbar key={location.pathname} />
+  ) : (
+    <UnauthorizedNavbar />
   );
 };
