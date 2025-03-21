db5b541c6d9413c71a9013d153511579ede32de9
valtosw <volodijbessarabov@knu.ua>
2025-03-21 20:15:16 +0200

    feat: improve layout structure and readability of CustomerProfilePage component

--- src/components/ProfileComponents/CustomerProfile.tsx
+++ src/components/ProfileComponents/CustomerProfile.tsx
@@ -58,54 +58,54 @@ const CustomerProfilePage = () => {
 
   return (
     <>
-  <Navbar />
-  <div className="p-6 space-y-6">
-    <div className="flex items-center justify-between w-full">
-      <div className="flex items-center space-x-4">
-        <Avatar size="lg" src={user?.profilePicture} />
-        <h2 className="text-xl font-semibold">{user?.fullName}</h2>
-      </div>
-      <Button>Edit Profile</Button>
-    </div>
-    <Divider className="w-full" />
-    <div className="flex h-screen">
-      {/* Statistics Section - Takes 3/4 of the width */}
-      <div className="flex-1 pr-6">
-        <h3 className="text-lg font-semibold">My Statistics</h3>
-        <div className="grid grid-cols-3 gap-6 h-full">
-          {/* Line Chart - 2/3 width */}
-          <div className="col-span-2 p-4 bg-gray-100 rounded-xl">
-            <Select
-              label="Select Exercise"
-              onChange={(e) => setSelectedExercise(e.target.value)}
-            >
-              {exercises.map((exercise) => (
-                <SelectItem key={exercise}>{exercise}</SelectItem>
+      <Navbar />
+      <div className="p-6 space-y-6">
+        <div className="flex items-center justify-between w-full">
+          <div className="flex items-center space-x-4">
+            <Avatar size="lg" src={user?.profilePicture} />
+            <h2 className="text-xl font-semibold">{user?.fullName}</h2>
+          </div>
+          <Button>Edit Profile</Button>
+        </div>
+        <Divider className="w-full" />
+        <div className="flex h-screen">
+          {/* Statistics Section - Takes 3/4 of the width */}
+          <div className="flex-1 pr-6">
+            <h3 className="text-lg font-semibold">My Statistics</h3>
+            <div className="grid grid-cols-3 gap-6 h-full">
+              {/* Line Chart - 2/3 width */}
+              <div className="col-span-2 p-4 bg-gray-100 rounded-xl">
+                <Select
+                  label="Select Exercise"
+                  onChange={(e) => setSelectedExercise(e.target.value)}
+                >
+                  {exercises.map((exercise) => (
+                    <SelectItem key={exercise}>{exercise}</SelectItem>
+                  ))}
+                </Select>
+              </div>
+            </div>
+          </div>
+
+          {/* Vertical Divider */}
+          <Divider className="h-screen" orientation="vertical" />
+
+          {/* Trainers Section - Takes 1/4 of the width */}
+          <div className="w-60 pl-6">
+            <h3 className="text-lg font-semibold pb-4">My Trainers</h3>
+            <div className="flex flex-col space-y-4 items-start">
+              {trainers.map((trainer) => (
+                <User
+                  key={trainer.firstName + trainer.lastName}
+                  avatarProps={{ src: trainer.photoUrl }}
+                  name={trainer.firstName + " " + trainer.lastName}
+                />
               ))}
-            </Select>
+            </div>
           </div>
         </div>
       </div>
-
-      {/* Vertical Divider */}
-      <Divider className="h-screen" orientation="vertical" />
-
-      {/* Trainers Section - Takes 1/4 of the width */}
-      <div className="w-60 pl-6">
-        <h3 className="text-lg font-semibold pb-4">My Trainers</h3>
-        <div className="flex flex-col space-y-4 items-start">
-          {trainers.map((trainer) => (
-            <User
-              key={trainer.firstName + trainer.lastName}
-              avatarProps={{ src: trainer.photoUrl }}
-              name={trainer.firstName + " " + trainer.lastName}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
-  </div>
-</>
+    </>
   );
 };
 
