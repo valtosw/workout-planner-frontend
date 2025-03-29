bc959e4a5bb20ebeb5149fc08963ae193781364a
valtosw <volodijbessarabov@knu.ua>
2025-03-29 11:39:04 +0200

    feat: add TrainerProfileModal component for displaying trainer details

---
+++ src/components/TrainerComponents/TrainerModal.tsx
@@ -0,0 +1,24 @@
+import { Modal } from "@heroui/react";
+
+import { Trainer } from "@/types/trainer";
+
+interface TrainerProfileModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  trainer: Trainer;
+}
+
+export const TrainerProfileModal = ({
+  isOpen,
+  onClose,
+  trainer,
+}: TrainerProfileModalProps) => {
+  return (
+    <Modal
+      className="bg-white dark:bg-[#18181c]"
+      isOpen={isOpen}
+      size="lg"
+      onClose={onClose}
+    />
+  );
+};
