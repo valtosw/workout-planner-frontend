57467d4c43676f46c79c6150b5852b6b917fc19a
valtosw <volodijbessarabov@knu.ua>
2025-04-05 14:49:24 +0300

    feat: integrate SettingsDropdown into WorkoutPlanCard for enhanced functionality

--- src/components/WorkoutPlanComponents/WorkoutPlanCard.tsx
+++ src/components/WorkoutPlanComponents/WorkoutPlanCard.tsx
@@ -12,6 +12,7 @@ import { WorkoutPlanEntry, WorkoutPlanProps } from "../../types/workout-plan";
 
 import axios from "@/api/axios";
 import { errorToast } from "@/types/toast";
+import { SettingsDropdown } from "./SettingsDropdown";
 
 const EntryToDisplay: React.FC<{ entry: WorkoutPlanEntry }> = ({ entry }) => {
   return (
@@ -53,75 +54,18 @@ const EntryToDisplay: React.FC<{ entry: WorkoutPlanEntry }> = ({ entry }) => {
 };
 
 export const WorkoutPlan: React.FC<WorkoutPlanProps> = ({ workoutPlan }) => {
-  const handleDownloadPdf = async () => {
-    try {
-      const response = await axios.get(
-        `/WorkoutPlan/${workoutPlan.id}/DownloadPdf`,
-        {
-          responseType: "blob",
-        },
-      );
-
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const a = document.createElement("a");
-
-      a.href = url;
-      a.download = `${workoutPlan.name}.pdf`;
-      document.body.appendChild(a);
-      a.click();
-      document.body.removeChild(a);
-      window.URL.revokeObjectURL(url);
-    } catch (error: any) {
-      errorToast("Failed to download PDF");
-    }
-  };
-
   return (
-    <Card className="min-w-[280px] max-w-sm min-h-[550px] flex flex-col border border-gray-300 dark:border-gray-600 hover:border-gray-500 dark:hover:border-white rounded-xl bg-background transition-all p-6">
-      <CardHeader>
-        <h1 className="text-xl font-semibold text-left">{workoutPlan.name}</h1>
+    <Card className="min-w-[280px] max-w-sm h-[550px] flex flex-col border border-gray-300 dark:border-gray-600 hover:border-gray-500 dark:hover:border-white rounded-xl bg-background transition-all p-6">
+      <CardHeader className="flex justify-between items-center">
+        <h1 className="text-xl font-semibold">{workoutPlan.name}</h1>
+        <SettingsDropdown workoutPlan={workoutPlan} />
       </CardHeader>
 
-      <CardBody className="flex flex-col gap-3 max-h-[250px] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-800">
+      <CardBody className="flex flex-col gap-3 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-800 max-h-[400px]">
         {workoutPlan.workoutPlanEntries.map((entry, index) => (
           <EntryToDisplay key={index} entry={entry} />
         ))}
       </CardBody>
-
-      <CardFooter className="flex flex-col gap-4">
-        <div className="flex justify-between w-full">
-          <div className="flex flex-col items-center">
-            <h1 className="text-sm text-gray-400">Created By:</h1>
-            <User
-              avatarProps={{
-                src: workoutPlan.createdBy.profilePicture ?? undefined,
-              }}
-              description={workoutPlan.createdBy.email}
-              name={`${workoutPlan.createdBy.firstName} ${workoutPlan.createdBy.lastName}`}
-            />
-          </div>
-
-          {workoutPlan.assignedTo !== null && (
-            <div className="flex flex-col items-center">
-              <h1 className="text-sm text-gray-400">Assigned To:</h1>
-              <User
-                avatarProps={{
-                  src: workoutPlan.assignedTo.profilePicture ?? undefined,
-                }}
-                description={workoutPlan.assignedTo.email}
-                name={`${workoutPlan.assignedTo.firstName} ${workoutPlan.assignedTo.lastName}`}
-              />
-            </div>
-          )}
-        </div>
-
-        <div className="flex flex-col w-full gap-2">
-          <Button className="w-full">View Workout Plan</Button>
-          <Button className="w-full" onPress={handleDownloadPdf}>
-            Download PDF
-          </Button>
-        </div>
-      </CardFooter>
     </Card>
   );
 };
