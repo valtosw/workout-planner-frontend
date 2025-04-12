c015b02599e5f1eee3098d56c83f16b658d684fb
valtosw <volodijbessarabov@knu.ua>
2025-04-12 23:28:56 +0300

    refactor: simplify Reset button styling in TrainerFilter component

--- src/components/FilterComponents/Filter.tsx
+++ src/components/FilterComponents/Filter.tsx
@@ -208,14 +208,7 @@ const TrainerFilter: React.FC<FilterProps> = ({ onFilter }) => {
           >
             Apply
           </Button>
-          <Button
-            className="w-full bg-gray-200 dark:bg-gray-900 text-black dark:text-white
-            hover:bg-gray-300 dark:hover:bg-gray-800
-            transition-colors duration-200
-            py-2 px-4
-            focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50"
-            onPress={resetFilter}
-          >
+          <Button color="danger" variant="flat" onPress={resetFilter}>
             Reset
           </Button>
         </div>
